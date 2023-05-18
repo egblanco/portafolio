c01d4a1b2d2e18b8ae9d5648f37dc0734a2d859a
Edgar <egblanco.betasoft@gmail.com>
2023-05-17 20:36:52 -0500

    tag-manager

--- pages/index.tsx
+++ pages/index.tsx
@@ -84,11 +84,8 @@ export default function Home() {
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
                 stroke="currentColor"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" />
                 <line x1="5" y1="12" x2="19" y2="12" />
